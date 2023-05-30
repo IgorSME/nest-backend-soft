009b998b180b687572cd8c020c36a3a159d00d12
Igor Smelyanets <ismelyanets@yahoo.com>
2023-05-30 08:06:14 +0300

    add ApiProperty

--- src/user/dto/user.dto.ts
+++ src/user/dto/user.dto.ts
@@ -1,28 +1,37 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsEmail, IsString, MinLength } from 'class-validator';
 
+
 export class RegisterUserDto {
+  @ApiProperty({ example: 'john_snow', description: 'The username of the user' })
   @IsString()
   @MinLength(3)
   username: string;
 
+  @ApiProperty({ example: 'password123', description: 'The password of the user' })
   @IsString()
   @MinLength(6)
   password: string;
 
+  @ApiProperty({ example: 'john@example.com', description: 'The email address of the user' })
   @IsEmail()
   email: string;
 }
 
+
 export class LoginUserDto {
+  @ApiProperty({ example: 'password123', description: 'The password of the user' })
   @IsString()
   @MinLength(6)
   password: string;
 
+  @ApiProperty({ example: 'john@example.com', description: 'The email address of the user' })
   @IsEmail()
   email: string;
 }
 
 export class RefreshTokenDto {
+  @ApiProperty({ example: 'refresh_token_string', description: 'The refresh token' })
   @IsString()
   refreshToken: string;
 }
